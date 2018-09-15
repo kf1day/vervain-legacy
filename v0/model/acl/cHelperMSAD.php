278d17cd1ce97cdc909d888b4aa85f27c69e50a1
Andrew Day <launchmyrocketsaway@gmail.com>
2018-09-15 15:30:32 +0300

    fix: cHelperMSAD not bounds to LDAP server until helper function is called

--- v0/model/acl/cHelperMSAD.php
+++ v0/model/acl/cHelperMSAD.php
@@ -1,20 +1,29 @@
 <?php namespace model\acl;
 
-class cHelperMSAD extends \model\db\cLDAP implements iHelper {
+class cHelperMSAD implements iHelper {
+
+	protected $pt = null;
+	protected $args = [];
+
+
+	public function __construct( $host, $port, $base, $user, $pass ) {
+		$this->args = [ $host, $port, $base, $user, $pass ];
+	}
 
 	public function get_user( string $uid ): rHelper {
+		if ( $this->pt === null ) $this->pt = new \model\db\cLDAP( $this->args[0], $this->args[1], $this->args[2], $this->args[3], $this->args[4] );
 		$filter = [
 			'objectClass' => 'user',
 			'objectCategory' => 'person',
 			'userPrincipalName' => $uid,
 		];
-		if ( $this->select( '', [ 'dn', 'displayName', 'objectGUID' ], $filter ) !== 1 ) return false;
-		$t = $this->fetch();
+		if ( $this->pt->select( '', [ 'dn', 'displayName', 'objectGUID' ], $filter ) !== 1 ) return false;
+		$t = $this->pt->fetch();
 
-		$this->select( '', [ 'dn', 'objectGUID', 'sAMAccountName' ], [ 'objectClass' => 'group', 'objectCategory' => 'group', 'member:1.2.840.113556.1.4.1941:' => $t[0] ] );
+		$this->pt->select( '', [ 'dn', 'objectGUID', 'sAMAccountName' ], [ 'objectClass' => 'group', 'objectCategory' => 'group', 'member:1.2.840.113556.1.4.1941:' => $t[0] ] );
 
 		$groups = [];
-		while ( $r = $this->fetch() ) {
+		while ( $r = $this->pt->fetch() ) {
 			$groups[] = $r;
 		}
 
